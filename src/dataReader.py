9d855875f7130fed4e4effcc50265f4d35893036
Alexander-Muendesi <u20426918@tuks.co.za>
2023-08-30 10:42:38 +0200

    ✨ Removed outliers from dataset

--- src/dataReader.py
+++ src/dataReader.py
@@ -1,19 +1,21 @@
-import random
 import pandas as pd
+from scipy.stats import zscore
 
-#For instances that have missing values, those input neurons will simply not activate for now
+# For instances that have missing values, those input neurons will simply not activate for now. Those instances are set to NaN
+# The Entity column has been removed as it is of little use. Instead will use latitude and longitude to indetift country
+# To detect outliers the z-score is calculated. Data points whose z score is out of 3 standard deviations are considered outliers
 class DataReader:
     def __init__(self, random_number_generator):
         self.filename = "./src/dataset/data.csv"
-        self.data = None
-        self.prediction_data = None
-        self.test_data = None
-        self.train_data = None
-        self.validation_data = None
-        self.random_number_generator = random_number_generator# seeded random number generator
-        self.training_indexs = {}
-        self.validation_indexs = {}
-        self.test_indexs = {}
+        self.data = None                            # DataFrame representing the entire file
+        self.prediction_data = None                 # DataFrame representing the prediction dataset
+        self.test_data = None                       # DataFrame representing the test dataset
+        self.train_data = None                      # DataFrame representing the training dataset
+        self.validation_data = None                 # DataFrame representing the validation dataset
+        self.random_number_generator = random_number_generator  # seeded random number generator
+        self.training_indexs = {}                   # keeps track of what indexes where used for the training dataset
+        self.validation_indexs = {}                 # keeps track of what indexes where used for the validation dataset
+        self.test_indexs = {}                       # Keeps track of what indexes where used for the test dataset
 
     def readFile(self):
         data = pd.read_csv(self.filename)
@@ -22,11 +24,30 @@ class DataReader:
         data.replace('', pd.NA, inplace=True)
         data[header_rows] = data[header_rows].apply(pd.to_numeric, errors="coerce")
         self.data = data
-        # print(self.data)
-        self.generate_prediction_data()
-        self.generate_training_dataset()
-        self.generate_validation_dataset()
-        self.generate_test_dataset()
+
+        # remove outliers from the dataset
+        self.remove_outliers()
+
+        # generate the various datasets and place them in a DataFrame
+        # self.generate_prediction_data()
+        # self.generate_training_dataset()
+        # self.generate_validation_dataset()
+        # self.generate_test_dataset()
+
+    # removes data points whose z score is more than 3 standard deviations 
+    def remove_outliers(self) :
+        data = self.data.copy()
+        del data["Entity"]
+        z_scores = data.apply(lambda column: zscore(column.dropna()), axis=0)       # calculate the z scores for each data point, excluding NaN values
+
+        threshold = 3
+        anomalies = (z_scores > threshold) | (z_scores < -threshold)
+
+        rows_without_anomalies = ~anomalies.any(axis=1)
+        indexes_without_anomalies = data.index[rows_without_anomalies]              
+        filtered_data = self.data.loc[indexes_without_anomalies]                    # keep only the rows that do not have anomalies in them
+        self.data = filtered_data
+
 
     # generates the test dataset
     def generate_test_dataset(self) :
@@ -41,8 +62,8 @@ class DataReader:
                 data.append(self.data.iloc[index].copy())
 
         data = pd.DataFrame(data, columns=self.data.columns)
+        del data["Entity"]
         self.test_data = data
-        print(self.test_data)
     
     #generates a validation set which consists of 10 % of the data
     def generate_validation_dataset(self) :
@@ -57,6 +78,7 @@ class DataReader:
                 data.append(self.data.iloc[index].copy())
 
         data = pd.DataFrame(data, columns=self.data.columns)
+        del data["Entity"]
         self.validation_data = data
 
     #generates a training set which consists of 70 % if the data
@@ -73,6 +95,7 @@ class DataReader:
                 data.append(self.data.iloc[index].copy())
 
         data = pd.DataFrame(data, columns=self.data.columns)
+        del data["Entity"]
         self.train_data = data    
 
     
@@ -106,6 +129,8 @@ class DataReader:
         #convert the list to a data frame
         data = pd.DataFrame(data, columns=south_africa_rows.columns)
         data.reset_index(drop=True,inplace=True)#reset the indexes in the data frame so first element has index of 0 again
+
+        #TODO migh have to add "del data["Entity"]" here but not sure. Keep this in mind
         self.prediction_data = data
 
 #Arbitrary notes
